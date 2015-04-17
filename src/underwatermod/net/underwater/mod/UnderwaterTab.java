e87bea21f0626d096b88f8666de9edf70ef20e66
dlsloan <dlsloan@ualberta.ca>
2015-04-17 17:13:47 -0600

    Revert "Revert "April 17""
    This reverts commit dd42e47ff2270584e7f8f7520b822c41961fb236.

---
+++ src/underwatermod/net/underwater/mod/UnderwaterTab.java
@@ -0,0 +1,18 @@
+package net.underwater.mod;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class UnderwaterTab extends CreativeTabs{
+
+	public UnderwaterTab(String label) {
+		super(label);
+	}
+
+	@Override
+	public Item getTabIconItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
